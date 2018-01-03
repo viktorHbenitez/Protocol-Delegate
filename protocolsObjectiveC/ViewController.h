7657749741b800a061a0196ebab9dd107048ed89
Victor <imac@iMac-de-Victor.local>
2018-01-03 16:29:52 -0600

    Initial Commit

---
+++ protocolsObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  protocolsObjectiveC
+//
+//  Created by Victor  on 03/01/18.
+//  Copyright © 2018 Victor . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ protocolsObjectiveC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  protocolsObjectiveC
+//
+//  Created by Victor  on 03/01/18.
+//  Copyright © 2018 Victor . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
